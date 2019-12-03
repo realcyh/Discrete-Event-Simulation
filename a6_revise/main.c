cb8f21de0147a7a57a412fcb3d1ae951210f641d
YH CHEN <realcyh@outlook.com>
2019-12-03 11:26:24 -0500

    Initial Commit

---
+++ a6_revise/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  a6_revise
+//
+//  Created by REAL CYH on 12/3/19.
+//  Copyright © 2019 REAL CYH. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
